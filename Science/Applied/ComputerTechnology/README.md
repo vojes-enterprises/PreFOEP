e66807bfba0cc3ce64ae3cce9d7ea372d9c81a11
cvoges12 <vogesclayton@gmail.com>
2019-08-27 20:25:10 -0500

    + Arts, eGPU, and work.sh

---
+++ Science/Applied/ComputerTechnology/README.md
@@ -0,0 +1,1 @@
+[eGPU](http://forum.notebookreview.com/threads/diy-egpu-experiences.418851/)
---
+++ work.sh
@@ -0,0 +1,4 @@
+# Common commands for working on FOEP
+# To remove such aliases,
+  # start a new terminal session
+alias new='read a; for i in $a; do mkdir $i; touch $i/README.md; done'
